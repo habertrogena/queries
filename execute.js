0a3aaf63881365dc62a5e3242c5927383f08417b
habertrogena <habertorogy12@gmail.com>
2024-08-26 18:24:25 +0300

    initial commit

---
+++ execute.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+// Read the SQL file containing queries and functions
+const sqlFile = path.join(__dirname, 'queries.sql');
+const sqlContent = fs.readFileSync(sqlFile, 'utf8');
+
+function executeSQL() {
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      process.exit(1);
+    }
+
+    connection.query(sqlContent, (error, results) => {
+      if (error) {
+        console.error('Error executing SQL:', error);
+        process.exit(1);
+      }
+
+      console.log('SQL executed successfully:', results);
+      connection.end();
+    });
+  });
+}
+
+executeSQL();
